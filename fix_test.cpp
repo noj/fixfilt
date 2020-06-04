7374f33bff8e9995c37d84821396ac5b79a4072b
Jon Olsson <jon.olsson@gmail.com>
2020-06-04 21:29:28 +0200

    Switch unit test framework from catch to doctest

--- fix_test.cpp
+++ fix_test.cpp
@@ -1,9 +1,10 @@
 // vim: ts=2 sw=2
 #include "fix.hpp"
-#include "catch/catch.hpp"
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
 #include <sstream>
 
-TEST_CASE ("Basic", "[Fix]")
+TEST_CASE ("fix basics")
 {
   {
     fix::Message msg ("", '|');
@@ -24,20 +25,20 @@ TEST_CASE ("Basic", "[Fix]")
   }
 }
 
-TEST_CASE ("get_field", "[Fix]")
+TEST_CASE ("fix get_field")
 {
   fix::Message msg ("8=FIX.4.4|35=8|49=ZONK|56=FOO|6=0|11=ABC123", '|');
   REQUIRE (msg.get_field (4711) == std::nullopt);
   REQUIRE (msg.get_field (35)->value == "8");
 }
 
-TEST_CASE ("parse_ver", "[Fix]")
+TEST_CASE ("fix parse_ver")
 {
   REQUIRE (fix::parse_ver ("foo") == std::nullopt);
   REQUIRE (*fix::parse_ver ("FIX.4.3") == fix::Ver::FIX43);
 }
 
-TEST_CASE ("SimpleExpand", "[Fix]")
+TEST_CASE ("fix SimpleExpand")
 {
   fix::Message msg ("8=FIX.4.3|35=A|43=N", '|');
 
